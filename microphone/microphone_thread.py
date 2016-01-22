6b0d17884bb34a69fe9702634d4cf041c669cb6f
Mishk0 <mshelemetev@gmail.com>
2016-01-21 22:31:39 -0500

    microphone class getting it shape out, need to fix microphone thread to allow it to be killed nicely. Also fixed broken English in README

---
+++ microphone/microphone_thread.py
@@ -0,0 +1,16 @@
+import threading
+ 
+class MicrophoneThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self):
+        super(MicrophoneThread, self).__init__()
+        self._stop = threading.Event()
+    
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+    
--- pyguard.py
+++ pyguard.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import sys, getopt, subprocess
 import os
+from queue import Queue
+import re
+from threading import Thread
+from microphone.microphone import Mic
+from pprint import pprint
 
 sys.path.insert(0, os.getcwd())
 
@@ -13,16 +18,22 @@ def main(argv):
         '''
          options:
           --email=test@test.com .. you should have mail server setup
+          or later I'll implement some ssl encrypted communication with my server which will push stuff there process and send to an email specified
+          ...
+          
+          Another option 
+          
           --dblevel=20 noise level
          '''
-  
+        
         
         for opt, arg in opts:
+        
             if opt in ('--help', '-h'):
                 print('HEEELLP')
                 sys.exit(0)
             
-            if opt == '--email' and is_email(arg) == true:
+            if opt == '--email' and re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", arg):
                 email = arg
             
             if opt == '--dblevel':
@@ -30,22 +41,40 @@ def main(argv):
 
                 
         '''
-        need to spin the threds andd get all the juzz up and running
+        need to spin the threads and get all the juzz up and running
         '''
+        # print('test')
+        q_microphone = Queue()        
+        microphone_thread = Thread(target=spin_microphone_listener, args=(q_microphone,))
+        microphone_thread.start()
         
     except getopt.GetoptError:
+        print('test1')
         sys.exit(2)
     except PermissionError:
+        print('test1')
         sys.exit(2)
     except Exception as e:
         print(e)
         sys.exit(2)
     except IndexError:
+        print('test1')
         sys.exit(2)
     except KeyboardInterrupt:
+        print('????test')
+        pprint(q_microphone)
+        pprint(q_microphone.get())
         sys.exit(1)
+    ''''
     except ImportError:
         sys.exit(2)    
+    '''
+def spin_microphone_listener(queue_mic):
+    mic = Mic(queue_mic)
+    print('!!!test!!!')
+    while True:
+        mic.listen()
         
+    
 if __name__ == "__main__":
     main(sys.argv[1:])
