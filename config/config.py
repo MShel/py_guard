7f9161c92510df040a45c9584ce7e9ea1b29ed5a
m <misha.shelemetyev@onforce.com>
2016-04-15 14:50:28 -0400

    just some more code for congif parser

--- config/config.py
+++ config/config.py
@@ -1,5 +1,25 @@
 import ConfigParser
 
-def get_config_object(filename :str):
-    parsed_config = ConfigParser.ConfigParser().read(filename)
-    return parsed_config
+class Config:
+    
+    def __init__(self, filename:str) -> dict:
+        res = []
+        raw_config_object = self.get_config_object(filename)
+        res = self.validate(raw_config_object)
+        return res
+    
+    def get_config_object(file :fp) -> dict:
+        parsed_config = ConfigParser.ConfigParser().readfp(filename)
+        return parsed_config
+   
+    '''
+    need to raise different 
+    exceptions:
+        warnings: (if emails is not specified)
+        error: (if picture directory is not writable, 
+        or microphone details are not ints )  
+    '''
+    def validate(self, raw_config_object):
+        validated_object = []
+        
+        return validated_object
