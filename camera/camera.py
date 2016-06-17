abe14e880aff51b6a0e2451653886636f711ef69
misha shelemetyev <misha.shelemetyev@onforce.com>
2016-06-17 13:05:18 -0400

    coroutine fun started+added video support... nothing tested

--- camera/camera.py
+++ camera/camera.py
@@ -1,19 +1,30 @@
 from cv2 import *
 from datetime import datetime
-
-from sentinels import Sentinel
+from threading import Timer
 
 
 # initialize the camera
 class Camera:
-    def __init__(self, queue, config_object: dict):
+    def __init__(self, config_object: dict):
         self.pictures_taken = 0
         self.camera = VideoCapture(0)
         self.pictures_directory = config_object["FILES"]["picture_directory"]
-        self.pictures_amount = int(float(config_object["CAMERA"]["pictures_amount"]))
-        self.queue = queue
+        self.pictures_amount = int(config_object["CAMERA"]["pictures_amount"])
+        self.video_time_interval = int(config_object["CAMERA"]["video_interval"])
+        self.camera_action = self._camera_action()
 
-    def takeNPictures(self):
+    @coroutine
+    def _camera_action(self):
+        while True:
+            args = (yield)
+            if args['action'] == 'photos':
+                self.take_some_pictures()
+            elif args['action'] == 'video':
+                self.record_some_video()
+            else:
+                print("Invalid action")
+
+    def take_some_pictures(self):
         for i in range(0, self.pictures_amount + 1):
             s, img = self.camera.read()
             if s:  # frame captured without any errors
@@ -21,9 +32,39 @@ class Camera:
                 picturePath = self.pictures_directory + str(datetime.today()) + ".jpg"
                 imwrite(picturePath, img)  # save image
                 self.pictures_taken += 1
-                #print("pictures taken "+self.pictures_taken+"\n")
-                if self.pictures_taken == self.pictures_amount:
-                    print("Put archiving Sentinel \n")
-                    sentinel = Sentinel(datetime.now(), Sentinel.archiveAction, picturePath)
-                    self.queue.put(sentinel)
-                    self.pictures_taken = 0
+                print("pictures taken " + self.pictures_taken + "\n")
+            if self.pictures_taken == self.pictures_amount:
+                self.pictures_taken = 0
+                self.camera.release()
+
+    def record_some_video(self):
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(self.pictures_directory + str(datetime.today()) + ".avi", fourcc, 20.0, (640, 480))
+        video_timer = Timer(self.video_time_interval, lambda done: print("timer done"))
+        video_timer.start()
+        while self.camera.isOpened() and video_timer.finished.is_set() is True:
+            ret, frame = self.camera.read()
+            if ret == True:
+                frame = cv2.flip(frame, 0)
+                # write the flipped frame
+                out.write(frame)
+                self.camera.release()
+                out.release()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    print('video recording failed')
+                    break
+            else:
+                print('video recording failed')
+                break
+
+    '''
+    proxy to couroutine
+    '''
+    def send(self,**kwargs):
+        self.camera_action.send(kwargs)
+
+    '''
+    close running coroutine
+    '''
+    def close(self)
+        self.takeNPictures.close()
